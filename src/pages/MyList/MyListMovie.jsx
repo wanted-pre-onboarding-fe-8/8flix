572865cd3345919512198f7b9a083546efba42e4
ohtaekwon <ohtk1119@gmail.com>
2022-07-07 18:49:03 +0900

    feat: 영화의 제목과 년도를 나타내는 컴포넌트 구현

--- src/pages/MyList/MyListMovie.jsx
+++ src/pages/MyList/MyListMovie.jsx
@@ -6,23 +6,40 @@ export default function MyListMovie({ poster }) {
     <>
       <Container>
         <Poster src={poster} />
+        <Description>
+          <Title>제목</Title>
+          <Year>Year</Year>
+        </Description>
       </Container>
     </>
   );
 }
 const Container = styled.div`
-  background-color: red;
+  background-color: green;
   --width: 300px;
   width: var(--width);
   height: calc(var(--width) * 3 / 2);
   margin: 10px;
   border-radius: 4px;
-  /* background-size: cover;
-  overflow: hidden; */
+  background-size: cover;
+  overflow: hidden;
 `;
 const Poster = styled.img`
   width: 100%;
   height: 80%;
   background-color: gray;
 `;
-const Info = styled.div``;
+const Description = styled.div`
+  width: 300px;
+  text-align: center;
+`;
+const Title = styled.h1`
+  background-color: red;
+  color: #000;
+  font-size: 16px;
+`;
+const Year = styled.h4`
+  background-color: red;
+  color: #e3e3e3;
+  width: 100%;
+`;
