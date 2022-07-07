4aa759842ab9c4168a4f325d8a98cd1421e4e225
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-07 15:56:31 +0900

    feat: 메인 페이지 그리드 레이아웃 추가
    데스크톱, 태블릿, 모바일로 브레이크 포인트 나눠서 대응

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useMovieModel } from '../../models/useMovieModel';
+import styled from 'styled-components';
 
 export default function Main() {
-  return <div>Main</div>;
+  const { movies, getMovies } = useMovieModel();
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <Container>
+      <Title>Main</Title>
+      <MovieSection>
+        {movies.map(({ id, type, year, title, poster }) => {
+          return <li key={id}>{title}</li>;
+        })}
+      </MovieSection>
+    </Container>
+  );
 }
+
+const Container = styled.main``;
+const Title = styled.h1`
+  text-align: center;
+`;
+const MovieSection = styled.section`
+  display: grid;
+  grid-gap: 1rem;
+  margin-top: 1rem;
+  padding: 0 1rem;
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: 767px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
