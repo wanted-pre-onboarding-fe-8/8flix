88c0c699dc98486f99c74b905b0c8ed90408a03d
corgi-world <developer.msw@gmail.com>
2022-07-07 11:47:31 +0900

    feat: 영화 요청 서비스 구현

---
+++ src/services/movieService.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { HttpRequest } from "../http/httpRequest";
+
+export const movieService = axios.create({ baseURL: "http://localhost:8000/movie" });
+
+export const movieRequest = new HttpRequest(movieService);
