62352b1f4a8241f77ab7033868bcf814ef52a1e4
corgi-world <developer.msw@gmail.com>
2022-07-08 00:07:53 +0900

    chore: 애니메이션 값 opacity로 변경

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -58,17 +58,17 @@ const Wrapper = styled.div`
 
 const fadeIn = keyframes`
   0% {
-    transform: scale(0);
+    opacity: 0;
   }
   100% {
-    transform: scale(1);
+    opacity: 1;
   }
 `;
 const fadeOut = keyframes`
   0% {
-    transform: scale(1);
+    opacity: 1;
   }
   100% {
-    transform: scale(0);
+    opacity: 0;
   }
 `;
