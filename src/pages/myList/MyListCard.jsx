1a7a23cb6ae3d8cd766396dcd1916d561db8954a
ChuYeonBin <cndusqls98@gmail.com>
2022-07-08 15:47:06 +0900

    style: MyList 컴포넌트 styling

--- src/pages/myList/MyListCard.jsx
+++ src/pages/myList/MyListCard.jsx
@@ -15,9 +15,10 @@ export default function MyListCard(/*{movie}*/) {
     year: 2017,
     rating: 9.4,
     runtime: 77,
-    genres: ['Action', 'asdasd', 'asddd'],
+    genres: ['Action', 'asdasd', 'asddd', 'asddd', 'asddd', 'asddd', 'asddd'],
     summary: '',
-    description_full: '',
+    description_full:
+      'asdkakopkpokopkoppommmlfmlv;mvl;xcopkopkopkopokpsdkopsaa',
     synopsis: '',
     yt_trailer_code: '',
     language: 'en',
@@ -66,3 +67,87 @@ export default function MyListCard(/*{movie}*/) {
     </CardWrapper>
   );
 }
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  color: white;
+  background-color: #181717;
+  border-radius: 5px;
+`;
+
+const Figure = styled.figure`
+  margin: 0;
+  overflow: hidden;
+  border-radius: 5px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 200px;
+  transition: 200ms;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+`;
+
+const Content = styled.div`
+  margin: 1rem;
+`;
+
+const Title = styled.p`
+  margin: 0;
+  margin-top: 6px;
+  font-size: 14px;
+`;
+
+const Description = styled.p`
+  font-size: 12px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Genres = styled.ul`
+  margin: 0;
+  margin-bottom: 1rem;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+`;
+
+const Genre = styled.li`
+  margin-right: 0.5rem;
+  font-size: 13px;
+`;
+
+const CardButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CardButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  color: white;
+  background-color: #181717;
+  border: 1px solid gray;
+  border-radius: 50%;
+  cursor: pointer;
+
+  &:hover {
+    border: 2px solid gray;
+  }
+
+  & > * {
+    font-size: 24px;
+  }
+`;
