8e43de50f311a50115a7e8b8258872cea017b67b
bebusl <bebus1998@naver.com>
2022-07-11 20:53:19 +0900

    feat: 영화 정보 관리 훅 구현

---
+++ src/models/useMovie.js
@@ -0,0 +1,50 @@
+import { useRequest } from '../http/useRequest';
+import { GET, PATCH, POST } from '../utils/constants/request';
+import { movieService } from '../http/testRequest';
+
+export function useMovie() {
+  const {
+    data: movies,
+    loading: isLoadedMovie,
+    error: loadMovieError,
+    httpRequest: getRequest,
+  } = useRequest(movieService, {
+    onError: (e) => {
+      //에러 발생 시 실행시킬 에러핸들러 등록.
+    },
+    onComplete: (res) => {
+      //요청/응답 성공 시 실행시킬 핸들러 등록. res에 응답정보 담겨 있음.
+    },
+  });
+
+  const { httpRequest: patchRequest } = useRequest(movieService, {
+    onComplete: () => {
+      getMovies();
+    },
+  });
+
+  const getMovies = () => {
+    getRequest(GET, '');
+  };
+
+  const patchMovieById = (id, data) => {
+    patchRequest(PATCH, '', id, data);
+  };
+
+  const searchMovies = (word) => {
+    getRequest(GET, `?q=${word}`);
+  };
+  const searchLikedMovies = () => {
+    getRequest(GET, '?like=true');
+  };
+
+  return {
+    movies,
+    isLoadedMovie,
+    loadMovieError,
+    getMovies,
+    patchMovieById,
+    searchMovies,
+    searchLikedMovies,
+  };
+}
