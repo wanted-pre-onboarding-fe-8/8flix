ab9f3bb4468a4c3078390f03db4bb7efe798d2e0
bebusl <bebus1998@naver.com>
2022-07-12 22:55:11 +0900

    feat: 기기의 theme 설정 가져오는 훅 구현

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+function useTheme() {
+  const [isLightMode, setIsLightMode] = useState(true);
+
+  const detectDarkMode = () => {
+    return (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    );
+  };
+  useEffect(() => {
+    if (detectDarkMode) {
+      setIsLightMode(false);
+    } else setIsLightMode(true);
+  }, []);
+
+  return isLightMode;
+}
+
+export default useTheme;
