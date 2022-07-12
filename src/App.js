6a4e050c89ec610b51ee1412d85328d47a7b893f
bebusl <bebus1998@naver.com>
2022-07-13 01:57:05 +0900

    feat: 페이지 오른쪽하단에 토글버튼 띄우기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import GNB from './components/GNB';
 import Router from './Router';
 import { useMovie } from './models/useMovie';
+import ToggleButton from './components/ToggleButton';
 
 function App() {
   const { getMovies } = useMovie();
@@ -14,6 +15,7 @@ function App() {
     <>
       <GNB />
       <Router />
+      <ToggleButton />
     </>
   );
 }
