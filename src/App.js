eae34166e3bba77cc9ed146b51967f6391f37e89
corgi-world <developer.msw@gmail.com>
2022-07-12 12:35:21 +0900

    feat: 첫 로드시 getMovies 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import GNB from './components/GNB';
 import Router from './Router';
+import { useMovie } from './models/useMovie';
 
 function App() {
+  const { getMovies } = useMovie();
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <>
       <GNB />
