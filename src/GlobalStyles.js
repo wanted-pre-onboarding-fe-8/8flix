edcd7975fbd54fabd1402da59540735f9e780ba8
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-13 11:34:45 +0900

    chore: pretendard 폰트 설치

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+<<<<<<< HEAD
  html, body {
      padding: 0;
      margin: 0;
@@ -11,6 +12,23 @@ const GlobalStyles = createGlobalStyle`
    outline:none;
    border:none;
  }
+=======
+body {
+  padding: 0;
+  margin: 0;
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
+    'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+}
+
+>>>>>>> 3503e3d (chore: pretendard 폰트 설치)
 `;
 
 export default GlobalStyles;
